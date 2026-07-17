d80893e19d33f28fd74bfd31cb728dcbe245a147
shadow <haybikeapps@gmail.com>
2026-07-17 21:32:41 +0700

    Add database configuration file

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+ $host = 'localhost';
+ $dbname = 'db_haygroup';
+ $user = 'root';
+ $pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi Gagal: " . $e->getMessage());
+}
+?>
